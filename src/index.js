7783b420b541e285c33fa8d83e13ec1f70121ff4
James Young <jamthoyoung@gmail.com>
2018-03-26 15:40:29 -0400

    More webpack config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import smile from './smile.png';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
+  // add image to our existin div
+  var myIcon = new Image();
+  myIcon.src = smile;
+  element.appendChild(myIcon);
+  
   return element;
 }
 
