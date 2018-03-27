964b5eff06f7db072461a6801eb90643c1cd3ec9
James Young <jamthoyoung@gmail.com>
2018-03-27 09:06:48 -0400

    Prepare project

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import smile from './smile.png';
-import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // add image to our existin div
-  var myIcon = new Image();
-  myIcon.src = smile;
-  element.appendChild(myIcon);
-
-  console.log('hahahah');
-  console.log(Data);
 
   return element;
 }
